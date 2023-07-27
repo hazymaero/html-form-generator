08b0f9b8d087a1a53dcb860da1dce44d0fa6e6e7
Maero <>
2023-07-27 14:37:54 -0400

    create form component

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,9 @@
-<h1>Hello World!</h1>
+<script>
+    import Form from "./components/Form.svelte";
+
+</script>
+<main>
+    <h1 style="color: var(--accent-color)">HTML Form Generator</h1>
+
+    <Form />
+</main>
