da2af35abb344953c3168a30d2fd3530b4e017b8
Maero <>
2023-07-27 15:19:53 -0400

    submit button

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -2,6 +2,10 @@
     import { InputType } from "../types";
 
     let inputType: InputType;
+
+    const handleSubmit = () => {
+
+    }
 </script>
 
 <div class="form-container">
@@ -11,4 +15,32 @@
         <option>Number</option>
         <option>Options</option>
     </select>
+
+    <center>
+        <button class="submit-button" on:click={handleSubmit}>Add</button>
+    </center>
 </div>
+
+<style>
+    .submit-button {
+        position: absolute;
+        bottom: 9em;
+    }
+
+    select {
+        outline: none;
+        padding: 0.5em;
+        color: var(--text-color);
+        background-color: rgb(30, 30, 30);
+        border-radius: 0.4em;
+        margin-left: 0.5em;
+        font-family: "Poppins";
+        border-color: transparent;
+
+        transition: all 0.1s ease-in;
+    }
+
+    select:hover {
+        filter: brightness(140%);
+    }
+</style>
