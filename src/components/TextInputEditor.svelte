4810621bf07cccfdf7f7a69f2b626d69153920be
Maero <>
2023-07-28 22:18:06 -0400

    styling for text input fields

--- src/components/TextInputEditor.svelte
+++ src/components/TextInputEditor.svelte
@@ -4,13 +4,36 @@
     export let label: string = "";
 </script>
 
-<div>
+<div class="main-content">
     <label for="idInput">ID for Input: </label>
     <input bind:value={id} type="text" id="idInput" />
+
+    <br>
 
     <label for="labelInput">Label: </label>
     <input bind:value={label} type="text" id="labelInput" />
 
+    <br>
+
     <label for="placeholderInput">Placeholder Text: </label>
     <input bind:value={placeholderText} type="text" id="placeholderInput" />
 </div>
+
+<style>
+    .main-content {
+        text-align: center;
+    }
+
+    #labelInput {
+        margin-left: 7.25em;
+    }
+
+    #placeholderInput {
+        margin-left: 0.75em;
+    }
+
+    input {
+        margin-left: 4.25em;
+        margin-top: 1em;
+    }
+</style>
