3ec73202e2cb89004c878ab94d75bf374680f7d2
Maero <>
2023-07-28 22:13:17 -0400

    create fields for text input

--- src/components/TextInputEditor.svelte
+++ src/components/TextInputEditor.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
     export let placeholderText: string = "";
+    export let id: string = "";
+    export let label: string = "";
 </script>
 
 <div>
+    <label for="idInput">ID for Input: </label>
+    <input bind:value={id} type="text" id="idInput" />
+
+    <label for="labelInput">Label: </label>
+    <input bind:value={label} type="text" id="labelInput" />
+
     <label for="placeholderInput">Placeholder Text: </label>
     <input bind:value={placeholderText} type="text" id="placeholderInput" />
 </div>
